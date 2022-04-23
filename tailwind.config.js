968285749a2e7a4dbcf4ac9106938977cc3021ab
Alba Casas <albacasasarzua1b@gmail.com>
2022-04-23 18:54:03 +0200

    Install tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  content: ["./src/**/*.{tsx}"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    colors: {
+      primary: "#000",
+      secondary: "#fff",
+      background: "#fff",
+      text: "#555",
+    },
+    fontFamily: {
+      body: ["Roboto", "sans"],
+      heading: ["Montserrat", "sans"],
+    },
+    spacing: {
+      1: "4px",
+      2: "8px",
+      3: "16px",
+      4: "24px",
+      5: "32px",
+      6: "48px",
+      7: "64px",
+      8: "80px",
+    },
+  },
+};
